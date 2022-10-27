371215622f4c1928d5cf43c54922afd6b7be738a
bmol5 <108373176+bmol5@users.noreply.github.com>
2022-10-27 01:02:19 -0400

    Update main.tf

--- intTerraform/main.tf
+++ intTerraform/main.tf
@@ -29,6 +29,21 @@ resource "aws_internet_gateway" "gw_1" {
   vpc_id = aws_vpc.test-vpc.id
 }
 
+resource "aws_route_table" "route_table1" {
+  vpc_id = aws_vpc.test-vpc.id
+ 
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw_1.id
+  }
+}
+ 
+resource "aws_route_table_association" "route-subnet1" {
+  subnet_id      = aws_subnet.subnet1.id
+  route_table_id = aws_route_table.route_table1.id
+}
+
+
 resource "aws_instance" "web_server01" {
   ami = "ami-08c40ec9ead489470"
   instance_type = "t2.micro"
