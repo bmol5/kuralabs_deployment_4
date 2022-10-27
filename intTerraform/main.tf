40dae08e3b3189735183af45389c8997b67ada02
bmol5 <108373176+bmol5@users.noreply.github.com>
2022-10-27 00:12:36 -0400

    VPC added
    Included VPC and subnet to host EC2 instance

--- intTerraform/main.tf
+++ intTerraform/main.tf
@@ -8,11 +8,28 @@ provider "aws" {
   
 }
 
+resource "aws_vpc" "test-vpc" {
+  cidr_block           = "172.25.0.0/16"
+  enable_dns_hostnames = "true"
+ 
+  tags = {
+    "Name" : "Deployment4VPC"
+  }
+}
+
+resource "aws_subnet" "public1" {
+  cidr_block = "172.25.0.0/18"
+  vpc_id                  = aws_vpc.test-vpc.id
+  map_public_ip_on_launch = "true"
+  availability_zone       = data.aws_availability_zones.available.names[0]
+}
+
 resource "aws_instance" "web_server01" {
   ami = "ami-08c40ec9ead489470"
   instance_type = "t2.micro"
   key_name = "ssh1"
   vpc_security_group_ids = [aws_security_group.web_ssh.id]
+  subnet_id = aws_subnet.subnet1.id  
 
   user_data = "${file("deploy.sh")}"
 
